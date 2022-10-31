2dc07460a50a82bc255f4690b1b76ac9cb86f51c
Jonas <jonas.haeutle@gmail.com>
2022-10-31 22:34:31 +0100

     debounce & throttle

--- functions/events.js
+++ functions/events.js
@@ -2,7 +2,7 @@ let events = {}
 events.send = (listener, msg) => {
   window.dispatchEvent(
     new CustomEvent(listener, {
-      detail: msg,
+      detail: msg
     })
   )
 }
@@ -11,6 +11,32 @@ events.receive = (listener, func) => {
   window.addEventListener(listener, (_event) => {
     func(_event.detail)
   })
+}
+
+events.debounce = (fn, wait) => {
+  let timer
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    const context = this
+    timer = setTimeout(() => {
+      fn.apply(context, args)
+    }, wait)
+  }
+}
+
+events.throttle = (fn, wait) => {
+  let throttled = false
+  return function (...args) {
+    if (!throttled) {
+      fn.apply(this, args)
+      throttled = true
+      setTimeout(() => {
+        throttled = false
+      }, wait)
+    }
+  }
 }
 
 export default events
